24f2c928b9325223e43c47a1b5543a906e86858b
Jonathan Ballauri <xteazz06@gmail.com>
2018-07-15 21:49:30 +0200

    Affichage des article rectifier

--- view/accueil.php
+++ view/accueil.php
@@ -38,7 +38,7 @@
         <img class="card-img-top" src="public/pictures/Alaska.jpg" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title"><?= $p['title'] ?></h5>
-          <p class="card-text"><?= $p['message'] ?></p>
+          <p class="card-text"><?= $p['message'] = mb_substr($p['message'], 0, strpos($p['message'], ' ', 100));?></p>
           <a class="btn btn-primary" href="?action=article&id=<?= $p['id'] ?>">Lire l'article</a>
         </div>
       </div>
@@ -48,9 +48,9 @@
     <section class="sectionmargin container" id="bio">
       <div class="jumbotron">
         <h1 class="display-4">Jean Forteroche</h1>
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+        <p class="lead">né le 21 juin 1948 à Łódź, est un écrivain polonais, auteur d'histoires fantastiques et de fantasy.</p>
         <hr class="my-4">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+        <p>Jean Forteroche a étudié l'économie et, avant de se mettre à l'écriture, a travaillé en tant que représentant de ventes senior pour une compagnie d'échanges internationaux. Sa première nouvelle, Le Sorceleur (Wiedźmin), fut publiée en 1986 dans Fantastyka, le magazine de littérature fantastique polonais de référence, et reçut un énorme succès de la part des critiques.</p>
         <a class="btn btn-primary btn-lg" href="?action=bio" role="button">Lire la suite</a>
       </div>
     </section>
--- view/updatescreen.php
+++ view/updatescreen.php
@@ -26,7 +26,7 @@
             <img class="card-img-top" src="public/pictures/Alaska.jpg" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title"><?= $p['title'] ?></h5>
-            <p class="card-text"><?= $p['message'] ?></p>
+            <p class="card-text"><?= $p['message'] = mb_substr($p['message'], 0, strpos($p['message'], ' ', 100));?></p>
             <a class="btn btn-warning" href="?action=updatepost&id=<?= $p['id'] ?>">Modifier l'article</a>
           </div>
         </div>
